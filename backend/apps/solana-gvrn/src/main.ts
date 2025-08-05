880128919ba2d9c152fcb06b2077c161b3efb430
Dru1DD <mrtproger@gmail.com>
2025-08-05 19:07:56 +0200

    feat: add swagger to backend

--- backend/apps/solana-gvrn/src/main.ts
+++ backend/apps/solana-gvrn/src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { ConfigService } from '@nestjs/config';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService: ConfigService = app.get(ConfigService);
+
+  const config = new DocumentBuilder()
+    .setTitle('Solana GVRN')
+    .setDescription('The API description')
+    .setVersion('1.0')
+    .addTag('cats')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, documentFactory);
 
   await app.enableCors();
 
